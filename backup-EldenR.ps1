43246f57c87fd82d2ee795bba2f51aaef597e74a
jordynarz <jpw31337@gmail.com>
2022-07-21 20:21:01 -0700

    Created a menu

--- backup-EldenR.ps1
+++ backup-EldenR.ps1
@@ -1,12 +1,96 @@
-$Date= ((Get-Date).ToString('MM-dd-yyyy-HH-mm'))
+<#
+Elden Ring Backup and Restore Script
+
+TODO: 
+	Contain the backup inside a function[COMPLETED]
+	Add Input for naming save folder [remove date?]
+	Input validation
+	Create Menu[Working but not what I want]
+	Create Restore functions
+		-able to view saved backups and select one
+
+#>
+
+using namespace System.Management.Automation.Host # Library for menu
+
+#$Date= ((Get-Date).ToString('MM-dd-yyyy-HH-mm'))					# Old Version
 $myDocs = [System.Environment]::GetFolderPath("MyDocuments")
 $appData = [System.Environment]::GetFolderPath("ApplicationData")
 $eldenPath = Join-Path -Path $appData -ChildPath "EldenRing"
 $docPath = Join-Path -Path $myDocs -ChildPath "EldenRingBackups"
-$savePath = Join-Path -Path $docPath -ChildPath $Date
+#$savePath = Join-Path -Path $docPath -ChildPath $Date   			#Old Version
+#$savePath = Join-Path -Path $docPath -ChildPath $inputBackup
 
-if (![System.IO.Directory]::Exists($eldenPath)) {
-	"The Path Does Not Exist."}
-else {
-	Get-ChildItem -Path $eldenPath -Include "*.sl2*" -Recurse | Compress-Archive -DestinationPath $savePath".zip" -Force
+function BackupSL2 {
+	if (![System.IO.Directory]::Exists($eldenPath)) {
+		"The Path Does Not Exist."}
+	else {
+		$inputBackup= Read-Host -Prompt "Name your backup: "
+		#INPUT VALIDATE THE ABOVE ^^^
+		$savePath = Join-Path -Path $docPath -ChildPath $inputBackup # Move to Input Menu
+
+		Get-ChildItem -Path $eldenPath -Include "*.sl2*" -Recurse | Compress-Archive -DestinationPath $savePath".zip" -Force
+
+		New-Menu # Calls the menu again to keep the program open
+	}
+	
 }
+
+function restoreSL2 {
+	Write-Host 'INSERT RESTORE FUNCTIONS HERE'
+
+	New-Menu #Runs the program menu again to keep the program open
+}
+
+function New-Menu {
+    #[CmdletBinding()]
+	Write-Host 	'------Elden Ring Backup Script------' -ForegroundColor Yellow
+	Write-Host "------What would you like to do?------" -ForegroundColor Green
+    $backup = [ChoiceDescription]::new('&Backup', 'Backup Elden Ring .sl2 files')
+    $restore = [ChoiceDescription]::new('&Restore', 'Restore the selected .sl2 file')
+    $quit = [ChoiceDescription]::new('&Quit', 'Quit out')
+
+    $options = [ChoiceDescription[]]($backup, $restore, $quit)
+
+    $result = $host.ui.PromptForChoice($Title, $Question, $options, 2)
+
+
+    switch ($result) {
+        0 { BackupSL2}
+        1 { restoreSL2 }
+        2 { return }
+    }
+	
+}
+
+function Show-Menu {
+    param (
+        [string]$Title = 'Elden Ring Backup Script'
+    )
+    Clear-Host
+    Write-Host "================ $Title ================"
+    
+    Write-Host "1: Press '1' Launch Utility."
+    Write-Host "Q: Press 'Q' to quit."
+}
+
+<#
+do
+ {
+    Show-Menu
+    $selection = Read-Host "Please make a selection"
+    switch ($selection)
+    {
+    '1' {
+    New-Menu
+    } 'q' {
+    'Quitting'
+    }
+    }
+    pause
+ }
+ until ($selection -eq 'q')
+#>
+
+# Run the Program Menu
+New-Menu
